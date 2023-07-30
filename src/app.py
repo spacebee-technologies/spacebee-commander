eda1df2ee2e69a5f64e8dbefb41ace09e0be1b88
Lautaro Mendieta <lemendieta96@gmail.com>
2023-07-30 20:26:47 -0300

    Add new command to start move forward

--- src/app.py
+++ src/app.py
@@ -12,34 +12,6 @@ class RovertitoCommander(cmd.Cmd):
     intro = 'Welcome to RovertitoCommander. Type help or ? to list commands.\n'
     prompt = '$ '
 
-    def do_set_target_velocity(self, arg):
-        target_velocity = float(arg)
-        timestamp = 11
-        interaction_type = 2  # SUBMIT
-        interaction_stage = 1
-        transaction_id = 14
-        service = 1  # TC
-        operation = 4  # SetTargetVelocity telecommand
-        area_version = 0
-        is_error_message = 0
-        body_length = 4
-        header = struct.pack('<QHBQHHHBH', timestamp,
-                                        interaction_type,
-                                        interaction_stage,
-                                        transaction_id,
-                                        service,
-                                        operation,
-                                        area_version,
-                                        is_error_message,
-                                        body_length)
-        body = struct.pack('f', target_velocity)
-        crc = bytes.fromhex('0000')
-        message = header + body + crc
-        socket_file_descriptor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        socket_file_descriptor.sendto(message, (ROVER_IP, ROVER_PORT))
-        socket_file_descriptor.close()
-        print(f'Sent: {message.hex()}')
-
     def do_set_mode(self, arg):
         mode = int(arg)
         timestamp = 11
@@ -68,17 +40,17 @@ class RovertitoCommander(cmd.Cmd):
         socket_file_descriptor.close()
         print(f'Sent: {message.hex()}')
 
-    def do_set_profile_mode(self, arg):
-        profile_mode = int(arg)
+    def do_set_height(self, arg):
+        angle_position = float(arg)
         timestamp = 11
         interaction_type = 2  # SUBMIT
         interaction_stage = 1
         transaction_id = 14
         service = 1  # TC
-        operation = 3  # SetProfileMode telecommand
+        operation = 3  # SetHeight telecommand
         area_version = 0
         is_error_message = 0
-        body_length = 1
+        body_length = 4
         header = struct.pack('<QHBQHHHBH', timestamp,
                                         interaction_type,
                                         interaction_stage,
@@ -88,7 +60,62 @@ class RovertitoCommander(cmd.Cmd):
                                         area_version,
                                         is_error_message,
                                         body_length)
-        body = profile_mode.to_bytes(1, 'little')
+        body = struct.pack('f', angle_position)
+        crc = bytes.fromhex('0000')
+        message = header + body + crc
+        socket_file_descriptor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        socket_file_descriptor.sendto(message, (ROVER_IP, ROVER_PORT))
+        socket_file_descriptor.close()
+        print(f'Sent: {message.hex()}')
+
+    def do_set_target_velocity(self, arg):
+        target_velocity = float(arg)
+        timestamp = 11
+        interaction_type = 2  # SUBMIT
+        interaction_stage = 1
+        transaction_id = 14
+        service = 1  # TC
+        operation = 4  # SetTargetVelocity telecommand
+        area_version = 0
+        is_error_message = 0
+        body_length = 4
+        header = struct.pack('<QHBQHHHBH', timestamp,
+                                        interaction_type,
+                                        interaction_stage,
+                                        transaction_id,
+                                        service,
+                                        operation,
+                                        area_version,
+                                        is_error_message,
+                                        body_length)
+        body = struct.pack('f', target_velocity)
+        crc = bytes.fromhex('0000')
+        message = header + body + crc
+        socket_file_descriptor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        socket_file_descriptor.sendto(message, (ROVER_IP, ROVER_PORT))
+        socket_file_descriptor.close()
+        print(f'Sent: {message.hex()}')
+
+    def do_start_forward(self, arg):
+        timestamp = 11
+        interaction_type = 2  # SUBMIT
+        interaction_stage = 1
+        transaction_id = 14
+        service = 1  # TC
+        operation = 5  # StartForward telecommand
+        area_version = 0
+        is_error_message = 0
+        body_length = 0
+        header = struct.pack('<QHBQHHHBH', timestamp,
+                                        interaction_type,
+                                        interaction_stage,
+                                        transaction_id,
+                                        service,
+                                        operation,
+                                        area_version,
+                                        is_error_message,
+                                        body_length)
+        body = bytes()
         crc = bytes.fromhex('0000')
         message = header + body + crc
         socket_file_descriptor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
