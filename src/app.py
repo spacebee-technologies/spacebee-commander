6f7932d7d78dada53496eeee2e12f81db09c61d5
Lautaro Mendieta <54816650+lautaromendieta@users.noreply.github.com>
2023-08-29 23:00:08 -0300

    Closes #5: "Add CRC and TM_Enabled" (#7)
    * Add TM_Enable and CRC in TC
    
    * Restore IP address from develop
    
    * Update operation numbers for TM enable and disable commands
    
    ---------
    
    Co-authored-by: burger1473 <fabianburgos.09@gmail.com>

--- src/app.py
+++ src/app.py
@@ -7,6 +7,20 @@ ROVER_IP = '192.168.0.228'
 ROVER_PORT = 51524
 
 
+def crc16_ccitt(data):
+    crc = 0xFFFF
+
+    for byte in data:
+        crc ^= (byte << 8)
+        for _ in range(8):
+            if crc & 0x8000:
+                crc = (crc << 1) ^ 0x1021
+            else:
+                crc <<= 1
+
+    return crc & 0xFFFF
+
+
 class RovertitoCommander(cmd.Cmd):
 
     intro = 'Welcome to RovertitoCommander. Type help or ? to list commands.\n'
@@ -33,7 +47,8 @@ class RovertitoCommander(cmd.Cmd):
                                         is_error_message,
                                         body_length)
         body = mode.to_bytes(1, 'little')
-        crc = bytes.fromhex('0000')
+        calculated_crc = crc16_ccitt(header + body)
+        crc = calculated_crc.to_bytes(2, 'little')
         message = header + body + crc
         socket_file_descriptor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         socket_file_descriptor.sendto(message, (ROVER_IP, ROVER_PORT))
@@ -61,7 +76,8 @@ class RovertitoCommander(cmd.Cmd):
                                         is_error_message,
                                         body_length)
         body = struct.pack('f', angle_position)
-        crc = bytes.fromhex('0000')
+        calculated_crc = crc16_ccitt(header + body)
+        crc = calculated_crc.to_bytes(2, 'little')
         message = header + body + crc
         socket_file_descriptor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         socket_file_descriptor.sendto(message, (ROVER_IP, ROVER_PORT))
@@ -89,7 +105,8 @@ class RovertitoCommander(cmd.Cmd):
                                         is_error_message,
                                         body_length)
         body = struct.pack('f', target_velocity)
-        crc = bytes.fromhex('0000')
+        calculated_crc = crc16_ccitt(header + body)
+        crc = calculated_crc.to_bytes(2, 'little')
         message = header + body + crc
         socket_file_descriptor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         socket_file_descriptor.sendto(message, (ROVER_IP, ROVER_PORT))
@@ -116,7 +133,8 @@ class RovertitoCommander(cmd.Cmd):
                                         is_error_message,
                                         body_length)
         body = bytes()
-        crc = bytes.fromhex('0000')
+        calculated_crc = crc16_ccitt(header + body)
+        crc = calculated_crc.to_bytes(2, 'little')
         message = header + body + crc
         socket_file_descriptor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         socket_file_descriptor.sendto(message, (ROVER_IP, ROVER_PORT))
@@ -143,8 +161,69 @@ class RovertitoCommander(cmd.Cmd):
                                         is_error_message,
                                         body_length)
         body = bytes()
-        crc = bytes.fromhex('0000')
+        calculated_crc = crc16_ccitt(header + body)
+        crc = calculated_crc.to_bytes(2, 'little')
         message = header + body + crc
+        socket_file_descriptor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        socket_file_descriptor.sendto(message, (ROVER_IP, ROVER_PORT))
+        socket_file_descriptor.close()
+        print(f'Sent: {message.hex()}')
+
+    def do_TM_Enable(self, arg):
+        TM = int(arg)
+        timestamp = 11
+        interaction_type = 2  # SUBMIT
+        interaction_stage = 1
+        transaction_id = 14
+        service = 1  # TC
+        operation = 7  # TMEnable telecommand
+        area_version = 0
+        is_error_message = 0
+        body_length = 2
+        header = struct.pack('<QHBQHHHBH', timestamp,
+                                        interaction_type,
+                                        interaction_stage,
+                                        transaction_id,
+                                        service,
+                                        operation,
+                                        area_version,
+                                        is_error_message,
+                                        body_length)
+        body0 = TM.to_bytes(1, 'little')
+        body1 = int(1).to_bytes(1, 'little')
+        calculated_crc = crc16_ccitt(header + body0 + body1)
+        crc = calculated_crc.to_bytes(2, 'little')
+        message = header + body0 + body1 + crc
+        socket_file_descriptor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        socket_file_descriptor.sendto(message, (ROVER_IP, ROVER_PORT))
+        socket_file_descriptor.close()
+        print(f'Sent: {message.hex()}')
+
+    def do_TM_Disable(self, arg):
+        TM = int(arg)
+        timestamp = 11
+        interaction_type = 2  # SUBMIT
+        interaction_stage = 1
+        transaction_id = 14
+        service = 1  # TC
+        operation = 8  # TMDisable telecommand
+        area_version = 0
+        is_error_message = 0
+        body_length = 2
+        header = struct.pack('<QHBQHHHBH', timestamp,
+                                        interaction_type,
+                                        interaction_stage,
+                                        transaction_id,
+                                        service,
+                                        operation,
+                                        area_version,
+                                        is_error_message,
+                                        body_length)
+        body0 = TM.to_bytes(1, 'little')
+        body1 = int(0).to_bytes(1, 'little')
+        calculated_crc = crc16_ccitt(header + body0 + body1)
+        crc = calculated_crc.to_bytes(2, 'little')
+        message = header + body0 + body1 + crc
         socket_file_descriptor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         socket_file_descriptor.sendto(message, (ROVER_IP, ROVER_PORT))
         socket_file_descriptor.close()
