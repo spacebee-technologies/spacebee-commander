4725440dd15674d7cba40693e4e102cffca0676d
Lautaro Mendieta <lemendieta96@gmail.com>
2025-11-02 10:48:01 -0300

    Use snake case for message manager attribute

--- spacebee_commander/commander.py
+++ spacebee_commander/commander.py
@@ -7,7 +7,7 @@ from spacebee_commander.telecommand_interface import TelecommandInterface
 class Commander:
 
     telecommands = load_commands().values()
-    messageManager = MessageManager()
+    message_manager = MessageManager()
 
     def __init__(self, transport: Communication):
         self.communication = transport
@@ -26,16 +26,16 @@ class Commander:
 
     def send(self, telecommand: TelecommandInterface):
         """Consists of a single message sent without expecting a response."""
-        message = self.messageManager.make_message(telecommand, InteractionType.SEND)
+        message = self.message_manager.make_message(telecommand, InteractionType.SEND)
         self.communication.send(message)
 
     def submit(self, telecommand: TelecommandInterface):
         """It consists of a message with an acknowledgement response. It return True if ACK is okay and False otherwise."""
-        message = self.messageManager.make_message(telecommand, InteractionType.SUBMIT)
+        message = self.message_manager.make_message(telecommand, InteractionType.SUBMIT)
         self.communication.send(message)
         response=self.communication.receive()
         if response != None:
-            ack=self.messageManager.unpack(response)
+            ack=self.message_manager.unpack(response)
 
             if ack:
                 print("ACK")
@@ -50,13 +50,13 @@ class Commander:
     def request(self, telecommand: TelecommandInterface):
         """In of a message with a response message. It returns the message if everything is okay, False otherwise."""
 
-        message = self.messageManager.make_message(telecommand, InteractionType.REQUEST)
+        message = self.message_manager.make_message(telecommand, InteractionType.REQUEST)
         self.communication.send(message)
 
         response = self.communication.receive()
 
         if response != None:
-            unpack_response = self.messageManager.unpack(response)
+            unpack_response = self.message_manager.unpack(response)
             if isinstance(unpack_response, bytes):
                 parsed_response = telecommand.parse_output_arguments(unpack_response)
                 print(f"Command response: {parsed_response}")
